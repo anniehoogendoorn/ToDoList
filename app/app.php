badb73ed74a481ea9007c01796659d4baebe01b1
Ike Mattice & jenna navratil <student@epicodus.com>
2015-08-05 13:46:12 -0700

    Add delete tasks to twig

--- app/app.php
+++ app/app.php
@@ -25,12 +25,9 @@
 
     });
 
-    $app->post("/delete_tasks", function(){
+    $app->post("/delete_tasks", function() use ($app){
         Task::deleteAll();
-        return "
-        <h1>List Cleared!</h1>
-        <p><a href='/'>Home</a></p>
-        ";
+        return $app['twig']->render('delete_tasks.html.twig');
     });
 
 return $app;
