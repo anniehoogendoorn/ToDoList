443ddf468255032acdd1c43279e49b969f0aae7c
Ike Mattice & jenna navratil <student@epicodus.com>
2015-08-05 11:17:40 -0700

    Added delete button

--- app/app.php
+++ app/app.php
@@ -4,7 +4,7 @@
 
     session_start();
     if (empty($_SESSION['list_of_tasks'])) {
-        $_SESSION{'list_of_tasks'] = array();
+        $_SESSION['list_of_tasks'] = array();
     }
 
     $app = new Silex\Application();
@@ -13,7 +13,16 @@
     $app->get("/", function(){
         $output = "";
 
-        foreach (Task::getAll() as $task) {
+        $all_tasks = Task::getAll();
+
+        if (!empty($all_tasks)) {
+            $output = $output . "
+                <h1>To Do List</h1>
+                <p>Here are all your tasks:</p>
+                ";
+        }
+
+        foreach ($all_tasks as $task) {
             $output = $output . "<p>" . $task->getDescription() . "</p>";
         }
 
@@ -25,6 +34,11 @@
                 <button type='submit'>Add task</button>
             </form>
         ";
+        $output .="
+            <form action='/delete_tasks' method='post'>
+                <button type='submit'>Delete</button>
+            </form>
+            ";
 
         return $output;
   });
@@ -38,7 +52,15 @@
             <p><a href='/'>View your list of things to do.</a></p>
             ";
 
-    })
+    });
+
+    $app->post("/delete_tasks", function(){
+        Task::deleteAll();
+        return "
+        <h1>List Cleared!</h1>
+        <p><a href='/'>Home</a></p>
+        ";
+    });
 
 return $app;
 
