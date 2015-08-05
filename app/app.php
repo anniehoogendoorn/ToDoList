eb7d33074e31ab42f1d321657e893f2769c0e8ba
Ike Mattice & jenna navratil <student@epicodus.com>
2015-08-05 12:18:54 -0700

    Move new task to Twig

--- app/app.php
+++ app/app.php
@@ -15,17 +15,13 @@
     $app->get("/", function() use ($app){
 
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
-        
+
   });
 
-    $app->post("/tasks", function(){
+    $app->post("/tasks", function() use ($app){
         $task = new Task($_POST['description']);
         $task->save();
-        return "
-            <h1>You created a task!</h1>
-            <p>" . $task->getDescription() . "</p>
-            <p><a href='/'>View your list of things to do.</a></p>
-            ";
+        return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
 
     });
 
