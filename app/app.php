06b079b7d73cdbfa7ce25598f8fcfc3f61dedf3e
Annie Hoogendoorn & Jenna Navratil <student@epicodus.com>
2015-08-17 17:14:24 -0700

    modify app.php, add 'tasks' variable to array in post function.

--- app/app.php
+++ app/app.php
@@ -1,8 +1,10 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
 
     $app = new Silex\Application();
+    $app["debug"] = true;
 
     $server = 'mysql:host=localhost;dbname=todo';
     $username = 'root';
@@ -15,21 +17,36 @@
     ));
 
     $app->get("/", function() use ($app){
+        $person = "will ferrel";
+        $person2 = 'linda';
 
-        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
-
-  });
-
-    $app->post("/tasks", function() use ($app){
-        $task = new Task($_POST['description']);
-        $task->save();
-        return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
+        return $app['twig']->render('index.html.twig', array('best_friend' => $person, 'mom' => $person2));
 
     });
 
+    $app->get("/tasks", function() use ($app) {
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
+    $app->post("/tasks", function() use ($app){
+        $task = new Task($_POST['description']);
+        $task->save();
+        return $app['twig']->render('tasks.html.twig', array('newtask' => $task, 'tasks' => Task::getAll()));
+    });
+
+    $app->get("/categories", function() use ($app) {
+         return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/categories", function() use ($app) {
+         $category = new Category($_POST['name']);
+         $category->save();
+         return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+
     $app->post("/delete_tasks", function() use ($app){
-        Task::deleteAll();
-        return $app['twig']->render('delete_tasks.html.twig');
+        Category::deleteAll();
+        return $app['twig']->render('index.html.twig');
     });
 
 return $app;
